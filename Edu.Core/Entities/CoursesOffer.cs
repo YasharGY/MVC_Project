0f7ff6bada55d3526d5a6c317b120467777fee44
YasharGY <yasharagu@code.edu.az>
2023-06-27 08:29:05 +0400

    Courses Offer Create succsesfully

--- Edu.Core/Entities/CoursesOffer.cs
+++ Edu.Core/Entities/CoursesOffer.cs
@@ -7,6 +7,7 @@ namespace Edu.Core.Entities;
 public class CoursesOffer:IEntity
 {
 	public int Id { get; set; }
+	public string ImagePath { get; set; }
 	[Required,MaxLength(30)]
 	public string Title { get; set; }
 	[Required,MaxLength(150)]

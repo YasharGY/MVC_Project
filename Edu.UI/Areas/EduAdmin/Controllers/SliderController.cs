1271f4e7a48afdb7d1072b27d02cb291d1602c18
YasharGY <yasharagu@code.edu.az>
2023-06-29 11:37:15 +0400

    Created SliderPost

---
+++ Edu.UI/Areas/EduAdmin/Controllers/SliderController.cs
@@ -0,0 +1,51 @@
+﻿using Edu.Core.Entities;
+using Edu.DataAccess.Contexts;
+using EduApp.Areas.EduAdmin.ViewModels.SliderViewModel;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace EduApp.Areas.EduAdmin.Controllers;
+[Area("EduAdmin")]
+
+public class SliderController : Controller
+{
+	private readonly AppDbContext _context;
+
+	public SliderController(AppDbContext context)
+	{
+		_context = context;
+	}
+
+	public async Task<IActionResult> Index()
+	{
+		return View(await _context.Sliders.ToArrayAsync());
+	}
+
+	public IActionResult Create()
+	{
+		return View();
+	}
+
+	[HttpPost]
+
+    public async Task<IActionResult> Create(SliderPostVM sliderpost)
+    {
+		if (!ModelState.IsValid)
+		{
+			return View();
+		}
+		Slider slider = new()
+		{
+			Title = sliderpost.Title,
+			Title2 = sliderpost.Title2,
+			Description = sliderpost.Description,
+			ImagePath = sliderpost.ImagePath
+
+		};
+		await _context.Sliders.AddAsync(slider);
+		await _context.SaveChangesAsync();
+
+        return RedirectToAction(nameof(Index));
+    }
+
+}
---
+++ Edu.UI/Areas/EduAdmin/ViewModels/SliderViewModel/SliderPostVM.cs
@@ -0,0 +1,16 @@
+﻿namespace EduApp.Areas.EduAdmin.ViewModels.SliderViewModel
+{
+    public class SliderPostVM
+    {
+        public string Title { get; set; } = null!;
+        public string Title2 { get; set; } = null!;
+        public string Description { get; set; } = null!;
+        public string ImagePath { get; set; } = null!;
+    }
+
+
+
+
+
+}
+

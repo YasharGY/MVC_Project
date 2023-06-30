8264edcf54767582c53372e9d4b3fd11fc9cf28b
YasharGY <yasharagu@code.edu.az>
2023-06-30 11:13:22 +0400

    Notices admin index created

---
+++ Edu.UI/Areas/EduAdmin/Controllers/NoticesController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EduApp.Areas.EduAdmin.Controllers;
+
+[Area("EduAdmin")]
+
+public class NoticesController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}

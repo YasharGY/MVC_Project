6ebe67b690c1ef8f22189fd3227d9f265324c282
YasharGY <yasharagu@code.edu.az>
2023-07-09 17:22:19 +0400

    Course Page Created

--- Edu.Core/Entities/Blog.cs
+++ Edu.Core/Entities/Blog.cs
@@ -8,6 +8,6 @@ public class Blog : IEntity
 	public int Id { get; set; }
 	public string ImagePath { get; set; }
 	public string ByWhom { get; set; }
-	public string Date { get; set; }
+	public DateTime Date { get; set; }
 	public string Description { get; set; }
 }
---
+++ Edu.UI/Controllers/CourseController.cs
@@ -0,0 +1,26 @@
+﻿using Edu.DataAccess.Contexts;
+using EduApp.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace EduApp.Controllers;
+
+public class CourseController : Controller
+{
+	private readonly AppDbContext _context;
+
+	public CourseController(AppDbContext context)
+	{
+		_context = context;
+	}
+
+	public async Task<IActionResult> Index()
+	{
+		HomeVM model = new HomeVM()
+		{
+			Courses = await _context.Courses.ToListAsync(),
+			
+		};
+		return View(model);
+	}
+}

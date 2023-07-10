c8831748d6906ca91a132ddcc9724b48f8e809b6
YasharGY <yasharagu@code.edu.az>
2023-07-10 19:45:25 +0400

    Auth page Created

---
+++ Edu.UI/Areas/EduAdmin/Controllers/AuthController.cs
@@ -0,0 +1,44 @@
+﻿using Edu.Core.Entities;
+using EduApp.Areas.EduAdmin.ViewModels.RegisterViewModel;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EduApp.Areas.EduAdmin.Controllers;
+[Area("EduAdmin")]
+public class AuthController : Controller
+{
+    private readonly UserManager<AppUser> _userManager;
+
+    public AuthController(UserManager<AppUser> userManager)
+    {
+        _userManager = userManager;
+    }
+
+    public IActionResult Register()
+    {
+        return View();
+    }
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Register(RegisterVM newuser)
+    {
+        if (!ModelState.IsValid) return View(newuser);
+        AppUser user = new()
+        {
+            Fullname = newuser.Fullname,
+            UserName = newuser.UserName,
+            Email = newuser.Email,
+
+        };
+        IdentityResult result= await _userManager.AddPasswordAsync(user,newuser.Password);
+        if (!result.Succeeded)
+        {
+            foreach (var error in result.Errors)
+            {
+                ModelState.AddModelError("", error.Description);
+            }
+            return View(newuser);
+        }
+        return Ok();
+    }
+}

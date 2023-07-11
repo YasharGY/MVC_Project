bddb6db5045cde5d52967defa192779b5489d4f1
YasharGY <yasharagu@code.edu.az>
2023-07-11 12:52:09 +0400

    Teacher Detaile is working witout bugs

---
+++ Edu.UI/Controllers/TeacherDetaileController.cs
@@ -0,0 +1,30 @@
+﻿using Edu.DataAccess.Contexts;
+using EduApp.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace EduApp.Controllers;
+
+public class TeacherDetaileController : Controller
+{
+	private readonly AppDbContext _context;
+	public TeacherDetaileController(AppDbContext context)
+	{
+		_context = context;
+	}
+
+	public async Task<IActionResult> Index(int id)
+	{
+		if (id == 0) BadRequest();
+		var teacher = await _context.teachers.FindAsync(id);
+		if (teacher is null) return NotFound();
+		ViewBag.TeacherId = teacher.Id;
+		
+
+		HomeVM model = new HomeVM
+		{
+			Teachers = await _context.teachers.Include(td => td.teacherDetaile).ToListAsync(),
+		};
+		return View(model);
+	}
+}

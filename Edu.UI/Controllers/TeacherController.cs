42d32e27bb57cc1c06211a5a370c672aeb56203a
YasharGY <yasharagu@code.edu.az>
2023-07-11 11:56:52 +0400

    Teacher Index Created

---
+++ Edu.UI/Controllers/TeacherController.cs
@@ -0,0 +1,28 @@
+﻿using Edu.Core.Entities;
+using Edu.DataAccess.Contexts;
+using EduApp.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace EduApp.Controllers;
+
+public class TeacherController : Controller
+{
+	public class TeachersController : Controller
+	{
+		private readonly AppDbContext _context;
+		public TeachersController(AppDbContext context)
+		{
+			_context = context;
+		}
+
+		public async Task<IActionResult> Index()
+		{
+			HomeVM model = new HomeVM
+			{
+				Teachers = await _context.teachers.ToListAsync()
+			};
+			return View(model);
+		}
+	}
+}

d3843c4dbcb481d6afe11633b95512c549efdc44
YasharGY <yasharagu@code.edu.az>
2023-07-17 01:44:15 +0400

    Blog Controller Created

---
+++ Edu.UI/Controllers/BlogController.cs
@@ -0,0 +1,25 @@
+﻿using Edu.DataAccess.Contexts;
+using EduApp.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace EduApp.Controllers;
+
+public class BlogController : Controller
+{
+	private readonly AppDbContext _context;
+
+	public BlogController(AppDbContext context)
+	{
+		_context = context;
+	}
+
+	public async Task<IActionResult> Index()
+	{
+		HomeVM homeVM = new HomeVM()
+		{
+			Blogs = await _context.Blogs.ToListAsync()
+		};
+		return View(homeVM);
+	}
+}

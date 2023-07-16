01449ee9efdb5dda4f0c801ec9c97d44b2f410b9
YasharGY <yasharagu@code.edu.az>
2023-07-17 02:08:59 +0400

    Blog Detaile is created

---
+++ Edu.UI/Controllers/BlogDetaileController.cs
@@ -0,0 +1,35 @@
+﻿using Edu.DataAccess.Contexts;
+using EduApp.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace EduApp.Controllers;
+
+public class BlogDetaileController : Controller
+{
+	private readonly AppDbContext _context;
+	public BlogDetaileController(AppDbContext context)
+	{
+		_context = context;
+	}
+	public async Task<IActionResult> Index(int id)
+	{
+		if (id == 0)
+		{
+			return NotFound();
+		}
+		var blog = await _context.Blogs.FindAsync(id);
+		if (blog == null)
+		{
+			return NotFound();
+		}
+		ViewBag.BlogId = blog.Id;
+
+		HomeVM homeVM = new()
+		{
+			Blogs = await _context.Blogs.ToListAsync(),
+			Categories = await _context.Categories.ToListAsync(),
+		};
+		return View(homeVM);
+	}
+}
